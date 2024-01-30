0cbc734e49242489ad4156ee2ac3631703fcbdff
ThomasM05 <158173202+ThomasM05@users.noreply.github.com>
2024-01-30 16:06:19 +0100

    Update App.py

--- App.py
+++ App.py
@@ -7,10 +7,8 @@ import base64
 
 # Function to select the folder containing the PDF files
 def select_folder():
-    folder_path = st.sidebar.file_uploader("Sélectionner le dossier contenant les fichiers PDF", type="pdf", accept_multiple_files=False)
-    if folder_path:
-        return folder_path.name, folder_path.getvalue()
-    return None, None
+    folder_path = st.sidebar.file_uploader("Sélectionner le dossier contenant les fichiers PDF", type="folder")
+    return folder_path
 
 def merge_pdfs(pdf_files):
     if not pdf_files:
@@ -18,8 +16,8 @@ def merge_pdfs(pdf_files):
         return
 
     merger = PdfMerger()
-    for pdf_file_name, pdf_file_content in pdf_files:
-        merger.append(pdf_file_content)
+    for pdf_file in pdf_files:
+        merger.append(pdf_file)
 
     output_file_path = "Fichier_Fusionne.pdf"
     with open(output_file_path, 'wb') as output_file:
@@ -30,17 +28,24 @@ def merge_pdfs(pdf_files):
 def main():
     st.title("Hello, Bienvenue sur votre nouvelle Plateforme...")
 
-    folder_name, folder_content = select_folder()
-    if folder_content:
-        merged_pdf_path = merge_pdfs([(folder_name, folder_content)])
-        if merged_pdf_path:
-            df_list = read_pdf(merged_pdf_path, encoding='ISO-8859-1', stream=True, area=[269.875, 12.75, 790.5, 961], guess=False, pages='all')
-            result_df = pd.concat(df_list, ignore_index=True)
-            excel_output_path = "Fichier_Extrait.xlsx"
-            result_df.to_excel(excel_output_path, index=False)
-            st.sidebar.success(f"Les Fichiers ont été enregistrées dans {excel_output_path}")
-            st.markdown("<h2 style='text-align: center;'>TELECHARGER LE FICHIER EXCEL ICI :)</h2>", unsafe_allow_html=True)
-            st.markdown(get_binary_file_downloader_html(excel_output_path, 'TELECHARGER'), unsafe_allow_html=True)
+    folder_path = select_folder()
+    if folder_path:
+        pdf_files = []
+        for file_name in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file_name)
+            if os.path.isfile(file_path) and file_path.lower().endswith('.pdf'):
+                with open(file_path, 'rb') as file:
+                    pdf_files.append(file)
+        if pdf_files:
+            merged_pdf_path = merge_pdfs(pdf_files)
+            if merged_pdf_path:
+                df_list = read_pdf(merged_pdf_path, encoding='ISO-8859-1', stream=True, area=[269.875, 12.75, 790.5, 961], guess=False, pages='all')
+                result_df = pd.concat(df_list, ignore_index=True)
+                excel_output_path = "Fichier_Extrait.xlsx"
+                result_df.to_excel(excel_output_path, index=False)
+                st.sidebar.success(f"Les Fichiers ont été enregistrées dans {excel_output_path}")
+                st.markdown("<h2 style='text-align: center;'>TELECHARGER LE FICHIER EXCEL ICI :)</h2>", unsafe_allow_html=True)
+                st.markdown(get_binary_file_downloader_html(excel_output_path, 'TELECHARGER'), unsafe_allow_html=True)
 
 # Function to create a download link for a file
 def get_binary_file_downloader_html(file_path, file_label='File'):
